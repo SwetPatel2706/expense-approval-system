bc91b6b4afdebf0b6ff93883438416f0ca123ad9
SwetPatel2706 <149137160+SwetPatel2706@users.noreply.github.com>
2026-01-12 19:51:28 +0530

    fix(expenses): zod error logging on server-side for expense creation validation

--- server/src/controllers/expenses.controller.js
+++ server/src/controllers/expenses.controller.js
@@ -28,29 +28,34 @@ export async function getExpenseById(req, res) {
 
 // POST /expenses
 export async function createExpense(req, res) {
-  const parsed = createExpenseSchema.safeParse(req.body);
-
-  if (!parsed.success) {
-    return res.status(400).json({
-      message: 'Invalid request body',
-      errors: parsed.error.errors,
+    const parsed = createExpenseSchema.safeParse(req.body);
+  
+    if (!parsed.success) {
+    //   console.log('❌ Zod validation failed');
+    //   console.log('Request body:', req.body);
+    //   console.log('Zod errors:', parsed.error.errors);
+    //--------------------------------
+  
+      return res.status(400).json({
+        message: 'Invalid request body',
+        errors: parsed.error.errors,
+      });
+    }
+  
+    const { amount, currency, category, userId } = parsed.data;
+  
+    const expense = await prisma.expense.create({
+      data: {
+        amount,
+        currency,
+        category,
+        userId,
+        status: 'PENDING',
+      },
     });
-  }
-
-  const { amount, currency, category, userId } = parsed.data;
-
-  const expense = await prisma.expense.create({
-    data: {
-      amount,
-      currency,
-      category,
-      userId,
-      status: 'PENDING',
-    },
-  });
-
-  res.status(201).json(expense);
-}
+  
+    res.status(201).json(expense);
+  }  
 
 // PUT /expenses/:id
 export async function updateExpense(req, res) {
